31d4e5b8277c1e96d06c33ffdeec0924310ccc68
Warren A Stowe <stowewa@gmail.com>
2020-08-09 18:52:55 -0400

    implemented vue router in place of navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,34 @@
 <template>
-    <div id="app">
-        <navigation @navSelection="currentDisplay = $event"></navigation>
-        <about v-if="currentDisplay == 'About'"></about>
-        <contact v-if="currentDisplay == 'Contact'"></contact>
-        <home v-if="currentDisplay == 'Home'"></home>
-        <projects v-if="currentDisplay == 'Projects'"></projects>
-    </div>
+  <div id="app">
+      <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link> |
+          <router-link to="/contact">Contact</router-link> |
+          <router-link to="/projects">Projects</router-link> |
+      </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-    import navigation from './components/Navigation.vue';
-    import about from './components/About.vue';
-    import contact from './components/Contact.vue';
-    import home from './components/Home.vue';
-    import projects from './components/Projects.vue';
-
-    export default {
-        name: 'App',
-        data: function () {
-            return {
-                currentDisplay: 'Home'
-            }
-        },
-        components: {
-            navigation,
-            about,
-            contact,
-            home,
-            projects
-        }
-    }
-</script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
